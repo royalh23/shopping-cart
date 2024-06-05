c74cc0497ed1096c89c96f974978f8c57c09c57d
Royal <royalhuseyn902@gmail.com>
2024-06-05 18:19:21 +0400

    Create routes.jsx and provide a router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import styles from './index.module.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './routes.jsx';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
