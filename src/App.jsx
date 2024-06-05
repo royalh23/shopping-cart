e56151cc95d0f02f934cf2f23d6706977c51ecd5
Royal <royalhuseyn902@gmail.com>
2024-06-05 22:38:53 +0400

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Outlet />
     </>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,7 @@
 import App from './App';
+import Home from './routes/Home';
+import ShoppingItems from './routes/ShoppingItems';
+import Cart from './routes/Cart';
 import ErrorPage from './routes/ErrorPage';
 
 const routes = [
@@ -6,6 +9,11 @@ const routes = [
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'shopping-items', element: <ShoppingItems /> },
+      { path: 'cart', element: <Cart /> },
+    ],
   },
 ];
 
