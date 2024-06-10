595867eba06feba2a26b602e7f5a2f597a49d810
Royal <royalhuseyn902@gmail.com>
2024-06-10 22:58:36 +0400

    Finish tests for ShoppingItems.jsx

---
+++ src/__tests__/ShoppingItems.test.jsx
@@ -0,0 +1,49 @@
+import { createMemoryRouter } from 'react-router-dom';
+import { vi } from 'vitest';
+import routes from '../routes';
+import { RouterProvider } from 'react-router';
+import { render, screen } from '@testing-library/react';
+
+vi.mock('../components/ShoppingItem.jsx', () => {
+  return {
+    default: () => <div>fetched item</div>,
+  };
+});
+
+vi.mock('../hooks/use-items.jsx', () => {
+  return {
+    default: vi
+      .fn()
+      .mockReturnValueOnce({ loading: true })
+      .mockReturnValueOnce({ error: 'Error' })
+      .mockReturnValueOnce({ items: Array.from({ length: 15 }, () => 0) }),
+  };
+});
+
+const entry = {
+  initialEntries: ['/shopping-items'],
+};
+
+it("Renders 'Loading...' when the API request is in progress", () => {
+  const router = createMemoryRouter(routes, entry);
+
+  render(<RouterProvider router={router} />);
+
+  expect(screen.getByText('Loading...')).toBeInTheDocument();
+});
+
+it("Renders 'Error' when there is an error", () => {
+  const router = createMemoryRouter(routes, entry);
+
+  render(<RouterProvider router={router} />);
+
+  expect(screen.getByText('Error')).toBeInTheDocument();
+});
+
+it('Renders 15 shopping items', () => {
+  const router = createMemoryRouter(routes, entry);
+
+  render(<RouterProvider router={router} />);
+
+  expect(screen.getAllByText('fetched item').length).toEqual(15);
+});
