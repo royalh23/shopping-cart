d44d04f9f7a390aa6cc29b24f05cd2f2ae6376d2
Royal <royalhuseyn902@gmail.com>
2024-07-03 23:45:12 +0400

    Use useMemo for calculating totalPrice and quantity of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
-  const cartItemsAmount = cartItems.reduce((prev, cur) => prev + cur.amount, 0);
+  const cartItemsAmount = useMemo(() => {
+    return cartItems.reduce((prev, cur) => prev + cur.amount, 0);
+  }, [cartItems]);
 
   return (
     <>
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,16 +1,20 @@
 import { useOutletContext } from 'react-router-dom';
 import styles from '../styles/Checkout.module.css';
 import Button from './Button';
+import { useMemo } from 'react';
 
 export default function Checkout() {
   const [cartItems] = useOutletContext();
+  const totalPrice = useMemo(() => {
+    return cartItems.reduce((prev, cur) => prev + cur.price * cur.amount, 0);
+  }, [cartItems]);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h2 className={styles.heading}>Total</h2>
         <div className={styles.total} data-testid="total-price">
-          ${cartItems.reduce((prev, cur) => prev + cur.price * cur.amount, 0)}
+          ${totalPrice}
         </div>
       </div>
       <p className={styles.para}>
